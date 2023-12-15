706eea4e2a80ca8b945a604c132bbdf257a68b27
Tomas <tomas.az@outlook.pt>
2023-12-15 03:35:32 +0000

    final sotre javascript

--- js/store.js
+++ js/store.js
@@ -1,80 +1,274 @@
-var buyButtons = document.querySelectorAll(".buyButton");
+var buyButton1 = document.getElementById("buyButton1");
+var buyButton2 = document.getElementById("buyButton2");
+var buyButton3 = document.getElementById("buyButton3");
+var buyButton4 = document.getElementById("buyButton4");
+var buyButton5 = document.getElementById("buyButton5");
+var buyButton6 = document.getElementById("buyButton6");
+var buyButton7 = document.getElementById("buyButton7");
+var buyButton8 = document.getElementById("buyButton8");
+var buyButton9 = document.getElementById("buyButton9");
 
-buyButtons.forEach(function(button){
-    button.addEventListener('click',function(event){
-        event.preventDefault();
-        validateQuantity();
+var feedbackMessage = document.getElementById("feedbackMessage");
+var feedbackMessage2 = document.getElementById("feedbackMessage2");
 
-        var feedbackMessage = document.getElementById("feedbackMessage");
-        var feedbackMessage2 = document.getElementById("feedbackMessage2");
-        var quantity1 = document.getElementById("quantitySelector1").value;
-        var quantity2 = document.getElementById("quantitySelector2").value;
-        var quantity3 = document.getElementById("quantitySelector3").value;
-        var quantity4 = document.getElementById("quantitySelector4").value;
-        var quantity5 = document.getElementById("quantitySelector5").value;
-        var quantity6 = document.getElementById("quantitySelector6").value;
-        var quantity7 = document.getElementById("quantitySelector7").value;
-        var quantity8 = document.getElementById("quantitySelector8").value;
-        var quantity9 = document.getElementById("quantitySelector9").value;
-        
-        if(validateQuantity() == 1){
-            var formData = {
-                "item1Text" : quantity1,
-                "item1Text" : quantity2,
-                "item1Text" : quantity3,
-                "item1Text" : quantity4,
-                "item1Text" : quantity5,
-                "item1Text" : quantity6,
-                "item1Text" : quantity7,
-                "item1Text" : quantity8,
-                "item1Text" : quantity9
-            };
-        
-            saveFormData(formData);
-        
-            feedbackMessage.style.display = 'block';
-        
-            fadeIn(feedbackMessage);
-            setTimeout(function () {
-                fadeOut(feedbackMessage);
-            }, 4000);
-        }else{
-            feedbackMessage2.style.display = 'block';
-        
-            fadeIn(feedbackMessage2);
-            setTimeout(function () {
-                fadeOut(feedbackMessage2);
-            }, 4000);
+buyButton1.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity1 = document.getElementById("quantitySelector1").value;
+
+    if(quantity1 != 0){
+        var storeData = {
+            "Item1" : quantity1
         }
 
-    });
-});
-    
-function validateQuantity(){
-    var validated = 1;
-    var quantity1 = document.getElementById("quantitySelector1").value;
-    var quantity2 = document.getElementById("quantitySelector2").value;
-    var quantity3 = document.getElementById("quantitySelector3").value;
-    var quantity4 = document.getElementById("quantitySelector4").value;
-    var quantity5 = document.getElementById("quantitySelector5").value;
-    var quantity6 = document.getElementById("quantitySelector6").value;
-    var quantity7 = document.getElementById("quantitySelector7").value;
-    var quantity8 = document.getElementById("quantitySelector8").value;
-    var quantity9 = document.getElementById("quantitySelector9").value;
-    
-    if(quantity1 == 0 && quantity2 == 0 && quantity3 == 0 && quantity4 == 0 && quantity5 == 0 && quantity6== 0 && quantity7== 0 && quantity8 == 0 && quantity9 == 0){  
-        validated = 0;
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
     }
+});
 
-    return validated;
-}
+buyButton2.addEventListener('click',function(event){
+    event.preventDefault();
 
-function saveFormData(formData) {
-    var storedFormData = JSON.parse(localStorage.getItem('formData')) || [];
+    var quantity2 = document.getElementById("quantitySelector2").value;
 
-    storedFormData.push(formData);
+    if(quantity2 != 0){
+        var storeData = {
+            "Item2" : quantity2
+        }
 
-    localStorage.setItem('formData', JSON.stringify(storedFormData));
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton3.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity3 = document.getElementById("quantitySelector3").value;
+
+    if(quantity3 != 0){
+        var storeData = {
+            "Item3" : quantity3
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton4.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity4 = document.getElementById("quantitySelector4").value;
+
+    if(quantity4 != 0){
+        var storeData = {
+            "Item4" : quantity4
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton5.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity5 = document.getElementById("quantitySelector5").value;
+
+    if(quantity5 != 0){
+        var storeData = {
+            "Item5" : quantity5
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton6.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity6 = document.getElementById("quantitySelector6").value;
+
+    if(quantity6 != 0){
+        var storeData = {
+            "Item6" : quantity6
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton7.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity7 = document.getElementById("quantitySelector7").value;
+
+    if(quantity7 != 0){
+        var storeData = {
+            "Item7" : quantity7
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton8.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity8 = document.getElementById("quantitySelector8").value;
+
+    if(quantity8 != 0){
+        var storeData = {
+            "Item8" : quantity8
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+buyButton9.addEventListener('click',function(event){
+    event.preventDefault();
+
+    var quantity9 = document.getElementById("quantitySelector9").value;
+
+    if(quantity9 != 0){
+        var storeData = {
+            "Item9" : quantity9
+        }
+
+        saveStoreData(storeData);
+        
+        feedbackMessage.style.display = 'block';
+        
+        fadeIn(feedbackMessage);
+        setTimeout(function () {
+            fadeOut(feedbackMessage);
+        }, 4000);
+    }else{
+        feedbackMessage2.style.display = 'block';
+        
+        fadeIn(feedbackMessage2);
+        setTimeout(function () {
+            fadeOut(feedbackMessage2);
+        }, 4000);
+    }
+});
+
+function saveStoreData(storeData) {
+    var storedStoreData = JSON.parse(localStorage.getItem('storeData')) || [];
+
+    storedStoreData.push(storeData);
+
+    localStorage.setItem('storeData', JSON.stringify(storedStoreData));
 }
 
 function fadeIn(msg) {
