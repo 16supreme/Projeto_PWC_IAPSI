172706475d770fa4166779bb8e9e377be38a0947
TiagoJ <tigasjustino@gmail.com>
2024-01-21 23:43:06 +0000

    Conexão à API a funcionar

--- js/CaesParaAdocao.js
+++ js/CaesParaAdocao.js
@@ -1,45 +1,59 @@
-fetch("https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}")
-    .then(function(res) {
-        console.log(res);
-    });
+fetch('https://api.petfinder.com/v2/animals?type=dog&limit=30', {
+  headers: {Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxazlyU0s2NUZlbXp2VUNDMUpHZWZNTUZwT1R4YmdYVE4zcFNxVUtFTFZMVDFhNFJaSiIsImp0aSI6IjZiNmVlMWIzMDNhMzEzYmEyNjA0NTE0MjMxOTI4NzBmMTY0NzUyNjNhNmE1MGVkMWExNjQyNWFkOTI5ZjQzODY2ZGFiYWIxYzEzNGFlMjMyIiwiaWF0IjoxNzA1ODgwMjU4LCJuYmYiOjE3MDU4ODAyNTgsImV4cCI6MTcwNTg4Mzg1OCwic3ViIjoiIiwic2NvcGVzIjpbXX0.WC2wjETAKQ0TzxTo5VXMt2EoMudp1T99ndKyvIAvfTJG2mVpjvc20bxNm8AV9lwa2I1Vgoea0K5sdD3vQaAsCa3W4Vm50qVHO-Q5NC_qjA4o6VKNgH9kh2WHkgfvhwBreT2jI_7D-SYlsAk-NEFjpLYNa6V0hQZ_i45TN1hNGWRmysZFSBSXYfnIcaIQdt8f_TyulGLKyrsJ0bfihrwHFtQs1H_lwF16uyBYIVH-zNUvVGQlCp3LrodvNgkSZNk9-HiSoKthgzIwOVe2M2mlYAln1_AV13k3pBkUA5ovyOEBQ0TVtqP4YFRd7nn3719VEGVZ4j2c6kPqJ2jghynTNQ'}
+})
+   .then(resp => resp.json())
+   .then(json => handleApiResponse(json))
+   
+   function handleApiResponse(response) {
+   //console.log(JSON.stringify(response));
+   console.info(`Api retornou ${response.animals.length}`)
+    response.animals.forEach(item => {
+        //console.log(JSON.stringify(item));
+        console.log(item.name);
+        createCard(item);    
+    }); 
 
-    var cloneCardAnimal = $(".card-animal").clone();
+    };
+    
 
-    cloneCardAnimal.apiKey ="1k9rSK65FemzvUCC1JGefMMFpOTxbgXTN3pSqUKELVLT1a4RZJ";
-    cloneCardAnimal.apiToken ="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxazlyU0s2NUZlbXp2VUNDMUpHZWZNTUZwT1R4YmdYVE4zcFNxVUtFTFZMVDFhNFJaSiIsImp0aSI6ImQzYmI3NzVkOGQ2ZmRiNTg0YjEyNTE1ZTkxMGUxNGVlMThjMzdhMjliNjc5OGRmMDc0MTU2NGEwNTBiYjIyZWUyOGFjYzA0YWExMWU2MmQyIiwiaWF0IjoxNzA1NzY4NTA3LCJuYmYiOjE3MDU3Njg1MDcsImV4cCI6MTcwNTc3MjEwNywic3ViIjoiIiwic2NvcGVzIjpbXX0.iZb8MEFOVuRfAF9h1AIZgspElPqtiEujiePbt9i6QekxrTqx0XK5ZHSdEujojV8oWQyC59jrmEopnGpanFZsM6PjGXIibSy9wj4W_DtSdzbQGDH3hwFQPZmq46HUp4QCWH7IRx78PXHKNRoSv_bf_4STmGFN0Lqdk62g0h9Ef8M-OOkWoNx6N5F8N3EhhhApvqweYcHGRR848Hl7Fhc0y3HhO1-yN_82nIT6th-JYYq4_za0NrgGwAnrQdXs6_vTcVknruK_wpH-1UIXNl9Z5mJDDDBu6b2dbbzmX5WugCbu7XnACamqKzk7fKA_f2xHogp3pa6gmGnSFIlmW9KThg";
+   function createCard(item) {
+    const div = document.createElement('div');
 
+    const photo = item.photos[0] ? item.photos[0].medium : "https://cdn.vectorstock.com/i/preview-1x/82/99/no-image-available-like-missing-picture-vector-43938299.jpg";
 
-pets.petsCall = function(userLocation, petType, petSex) {
-	console.log(userLocation, petType, petSex);
-	$.ajax({
-		url: pets.petUrl,
-		method: 'GET',
-    crossDomain: true,
-		dataType: 'jsonp',
-		data : {
-			key: pets.apiKey,
-			location: userLocation,
-      animal: petType,
-      sex: petSex,
-			format: 'json',
-			count: 10,
-			age: 'Senior',
-			status: 'A'
-		}  
-	}).then(function(results){
-  	var petResults = results.petfinder.pets.pet;
-		console.log(petResults);
-		for (var i = 0; i < petResults.length; ++i) {
-    	var petName = petResults[i].name.$t;
-      var petPhoto = petResults[i].media.photos.photo[0].$t;
-      console.log(petName);
-      console.log(petPhoto);
-    	pets.availablePets.append('<p>' + petName + '</p>');
-      pets.availablePets.append('<div><img src="' + petPhoto + '"></div>')
-  	}
-	});
-};
+    div.className = 'row';
 
-$(document).ready(function() {
-	pets.form();
-});
+    //console.log(item.photos[0].medium)
+
+    div.innerHTML = `
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+            <div class="card shadow-sm">
+                
+                    <img src="${photo}" class="card-img-top" alt="...">
+                    <div class="card-body" id="${item.id}">
+                        <h5 id="name">${item.name}</h5>
+                        <p id="description">${item.description}</p>
+                        <p id="type">E</p>
+                        <a href="#" onclick="adicionarFavorito(${item.id})" class="btn btn-primary">Adicionar aos favoritos</a>
+                    </div>
+                
+            </div>
+        </div>
+    </div>
+    `;
+  
+    document.getElementById('content').appendChild(div);
+   }
+ 
+   function adicionarFavorito(animalFavorito){
+    console.info("Adicionado aos favoritos");
+    var arrayFavoritos = [];
+    var dadosStorage = localStorage.getItem("listaAnimaisFavoritos");
+    if(dadosStorage !== null){
+        arrayFavoritos = JSON.parse(dadosStorage);
+    }
+    arrayFavoritos.push(animalFavorito);
+    var favoritosString = JSON.stringify(arrayFavoritos);
+    localStorage.setItem("listaAnimaisFavoritos",favoritosString);
+}   
